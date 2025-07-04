dd10de33e1a10fbfb45b3a02cfdfc9bacec1e075
Emanuelly Sena <147276660+phellowpy@users.noreply.github.com>
2025-07-04 11:04:38 -0300

    Create contato.py

---
+++ contato.py
@@ -0,0 +1,9 @@
+import random
+
+print("bem vindo ao contato!!")
+
+nome = input("Digite seu nome: ")
+email = input("Digite seu email: ")
+mensagem = input("Digite sua mensagem: ")
+
+print("Sua mensagem foi enviada!! Espere retorno.")
